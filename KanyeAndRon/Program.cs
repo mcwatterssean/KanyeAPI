5f6367b7554874c343ea586267862779a2bd0d10
Sean McWatters <mcwatterssean@gmail.com>
2021-12-26 06:32:19 -0500

    init

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace KanyeAndRon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                QuoteGenerator.KanyeQuote();
+                QuoteGenerator.RonQuote();
+            }
+        }
+    }
+}
